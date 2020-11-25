2499c93239b0a18b936fb01e81ea652599c3237a
saif420 <saifmahmud42069@gmail.com>
2020-11-25 08:39:23 +0600

    string

---
+++ String/string.9.2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    char country[] = {'b','a','n','g','l','a','d','e','s','h',' ','i','s',' ','m','y','\0','c','o','u','n','t','r','y'};
+
+
+    printf("%s\n",country);
+
+    return 0;
+}
+
